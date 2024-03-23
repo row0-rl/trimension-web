3ec0cb081f1cb84853fa0bba5ee206ca0e606f64
Row0 <Row0@outlook.com>
2024-03-23 13:17:29 -0500

    renamed project, initial updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,97 @@
+import { Client } from "appwrite";
 import Image from "next/image";
+import Navigation from "./nav";
+import Footer from "./footer";
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('652d954fcadd7f7f81b5');
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-black m-0">
+      <Navigation />
+      <section className="mt-16">
+        <div className="block mx-auto text-center mt-0 max-w-[940px]">
+          <img
+            src="/images/background.png"
+            loading="lazy"
+            sizes="(max-width: 767px) 100vw, 654.4609375px"
+            srcSet="/images/background-p-500.png 500w, /images/background-p-800.png 800w, /images/background-p-1080.png 1080w, /images/background-p-1600.png 1600w, /images/background.png 2211w"
+            alt=""
+            className="h-[600px] text-left inline-block"
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="text-center mt-[-163px] block relative">
+          <div className="text-white text-center text-xl static">About...</div>
+          <img
+            src="/images/trimension.png"
+            loading="lazy"
+            sizes="(max-width: 991px) 100vw, 804.84375px"
+            srcSet="/images/trimension-p-500.png 500w, /images/trimension-p-800.png 800w, /images/trimension-p-1080.png 1080w, /images/trimension.png 1515w"
+            alt=""
+            className="h-20 mt-20 inline-block"
+          />
+          <p className="h-24 text-gray-600 mt-9 ml-25 mr-25 pb-0 text-lg font-normal leading-relaxed">
+            Trimension is a wallpaper platform designed for Mac system. It not
+            only provides its own wallpapers, but also collects many well-known
+            wallpaper creators, and provides a safe and secure pre-sales and
+            after-sales platform for creators and lovers. Trimension introduced
+            a unified wallpaper display, users just upload a video, etc., can
+            get a live wallpaper display on the Mac desktop.{" "}
           </p>
-        </a>
-
+        </div>
+      </section>
+      <div className="flex justify-center items-center mt-16 mx-auto max-w-[940px]">
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="w-48 h-10 text-center mix-blend-normal bg-black text-white
+        bg-download rounded-2xl justify-center items-center flex shadow-download"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          Download for Mac
         </a>
       </div>
+      <section className="mt-32">
+        <div className="flex items-center mx-auto max-w-[940px]">
+          <img
+            src="/images/WechatIMG9950.jpeg"
+            loading="lazy"
+            width="680"
+            sizes="(max-width: 767px) 100vw, 680px"
+            alt=""
+            srcSet="/images/WechatIMG9950-p-500.jpeg 500w, images/WechatIMG9950-p-800.jpeg 800w, images/WechatIMG9950-p-1080.jpeg 1080w, images/WechatIMG9950.jpeg 1446w"
+          />
+          <div className="flex flex-col">
+            <a
+              href="#"
+              className="text-gray-200 mt-4 text-base no-underline hover:text-white"
+            >
+              Wallpapers
+            </a>
+            <a
+              href="#"
+              className="text-gray-200 mt-4 text-base no-underline hover:text-white"
+            >
+              Widgets
+            </a>
+            <a
+              href="#"
+              className="text-gray-200 mt-4 text-base no-underline hover:text-white"
+            >
+              Customize
+            </a>
+            <a
+              href="#"
+              className="text-[#8549ff] mt-7 no-underline hover:text-[#a478fc]"
+            >
+              View More...
+            </a>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,22 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "line": "radial-gradient(circle, rgba(255,255,255,0), #552dc2)",
+        "download": "radial-gradient(circle, rgba(11, 25, 129, .31), rgba(107, 16, 199, .98))",
+        "footer": "linear-gradient(rgba(68, 4, 124, .16), rgba(0, 0, 0, 0) 94%)",
+        "login": "radial-gradient(circle, rgba(71, 64, 148, .32), #000 250px)",
+        "button": "radial-gradient(circle, rgba(149, 81, 189, .51), rgba(213, 172, 255, .09))",
       },
+      backgroundColor: {
+        "input": "rgba(150, 150, 150, .1)",
+      },
+      boxShadow: {
+        "line": "0 0 19px 12px rgba(87, 26, 117, 0.42)",
+        "download": "0 2px 20px 5px rgba(245, 235, 255, .17)",
+      },
+      borderColor: {
+        "input": "rgba(190, 190, 190, .05)"
+      }
     },
   },
   plugins: [],
